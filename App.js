a0811b1462d2c3cb68723c1f9f6a2ec1884fe7c5
ABROUK BADRE <73764577+mrbadr1@users.noreply.github.com>
2023-05-07 21:11:56 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const [loaded] = useFonts({
+    'Exo-Bold': require('./assets/fonts/Exo-Bold.otf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
